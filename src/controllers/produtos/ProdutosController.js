8c7490f810240a47de715089ae7e12ffcea4d08e
LaurienSantos <lauriensanots768@gmail.com>
2024-07-14 12:39:54 -0300

    ajustes

--- src/controllers/produtos/ProdutosController.js
+++ src/controllers/produtos/ProdutosController.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
 import express from 'express';
 
-const token = 'b6da78c52b1d1a0faac6edeaeb2efa2f2b2c07e7';
-const url = process.env.URL;
 const router = express.Router();
 
 router.get('/produtos', (req, res) => {
-    axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+    axios.defaults.headers.common = {'Authorization': `Bearer ${process.env.TOKEN}`}
     async function request(){
-        console.log(token);
         try {
             const { data } = await axios.get(`${process.env.URL}/produtos?pagina=1&limite=100`);
             return res.status(200).send(data);            
@@ -20,10 +17,10 @@ router.get('/produtos', (req, res) => {
 });
 
 router.post('/produtos', (req, res) => {
-    axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+    axios.defaults.headers.common = {'Authorization': `Bearer ${process.env.TOKEN}`}
     async function request(){
         try {
-            const { data } = await axios.post(`${url}/produtos`, req.body);
+            const { data } = await axios.post(`${process.env.URL}/produtos`, req.body);
             return res.status(200).send(data);
         } catch (error) {
             return res.status(400).send(null);
@@ -33,11 +30,11 @@ router.post('/produtos', (req, res) => {
 });
 
 router.put('/produtos', (req, res) => {
-    axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+    axios.defaults.headers.common = {'Authorization': `Bearer ${process.env.TOKEN}`}
     async function request(){
         try {
             const idProduto = req.headers.idproduto;
-            const { data } = await axios.put(`${url}/produtos/${idProduto}`, req.body);
+            const { data } = await axios.put(`${process.env.URL}/produtos/${idProduto}`, req.body);
             return res.status(201).send(data);            
         } catch (error) {
             return res.status(400).send(null);
@@ -47,11 +44,11 @@ router.put('/produtos', (req, res) => {
 });
 
 router.delete('/produtos', (req, res) => {
-    axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+    axios.defaults.headers.common = {'Authorization': `Bearer ${process.env.TOKEN}`}
     async function request(){
         try {
             const idProduto = req.headers.idproduto;
-            const { data } = await axios.delete(`${url}/produtos/${idProduto}`);
+            const { data } = await axios.delete(`${process.env.URL}/produtos/${idProduto}`);
             return res.status(200).send(data);            
         } catch (error) {
             return res.status(400).send(null);
@@ -61,11 +58,11 @@ router.delete('/produtos', (req, res) => {
 });
 
 router.patch('/produtos/situacao', (req, res) => {
-    axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+    axios.defaults.headers.common = {'Authorization': `Bearer ${process.env.TOKEN}`}
     async function request(){
         try {
             const idProduto = req.headers.idproduto;
-            const { data } = await axios.patch(`${url}/produtos/${idProduto}/situacoes`, req.body);
+            const { data } = await axios.patch(`${process.env.URL}/produtos/${idProduto}/situacoes`, req.body);
             return res.status(200).send(data);            
         } catch (error) {
             return res.status(400).send(null);
